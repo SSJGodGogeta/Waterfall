47ebf9e8d168dde31cf458395cb8c5ac7a69d5da
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-11-03 23:30:12 +0100

    Removed interfaces as it would be a bit too much for this project. Added DatabaseService.ts to include functions to cache the database as needed.

--- Webpage/API/Interface/IPrivilege.ts
+++
@@ -1,7 +0,0 @@
-import {IRole} from "./IRole";
-
-export interface IPrivilege {
-    privilege_id: number;
-    privilege_techcode: string;
-    roles?: IRole[];
-}
--- Webpage/API/Interface/IUser.ts
+++
@@ -1,9 +0,0 @@
-import {IStaff} from "./IStaff";
-
-export interface IUser {
-    user_id: number;
-    user_email: string;
-    user_salt: string;
-    user_password: string;
-    staff: IStaff;
-}
