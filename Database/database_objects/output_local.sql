97ffa65f3dcc837a3913d7866f8fb5170263fa4c
Kevin Schmidhäusler <kevin.schmidh@icloud.com>
2024-11-04 17:46:47 +0100

    removed output_local.sql

--- Database/database_objects/output_local.sql
+++
@@ -1,8 +0,0 @@
-
-UPDATE SUMMARY
-Run:                          6
-Previously run:               4
-Filtered out:                 0
--------------------------------
-Total change sets:           10
-
