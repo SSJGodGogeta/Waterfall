d0d54755c5ad4c60246aed650156406348fc5380
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-11-06 20:11:34 +0100

    added a refresh button to clear the cache (using cacheRoute.ts). Once the cache is cleared, the current page refreshes automatically.

--- Webpage/API/app.ts
+++ Webpage/API/app.ts
@@ -8,6 +8,13 @@ import userRoutes from "./routes/userRoutes.js";
 import authenticationRoute from "./routes/authenticationRoute.js";
 // @ts-ignore
 import cookieParser from "cookie-parser";
+import {clearUserCache} from "./Service/UserService.js";
+import {clearRolesCache} from "./Service/RolesService.js";
+import {clearPrivilegeCache} from "./Service/PrivilegeService.js";
+import {clearProjectCache} from "./Service/ProjectService.js";
+import {clearStaffGroupCache} from "./Service/StaffGroupService.js";
+import {clearStaffCache} from "./Service/StaffService.js";
+import cacheRoute from "./routes/cacheRoute.js";
 
 const app = express();
 const PORT = 3000;
@@ -33,7 +40,20 @@ app.use("/api/authentication", authenticationRoute)
 app.use("/api/roles", roleRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/clearCache", cacheRoute)
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
+// Clearing all cache at once:
+export function clearAllCache(){
+    clearPrivilegeCache();
+    clearProjectCache();
+    clearRolesCache();
+    clearStaffGroupCache();
+    clearStaffCache();
+    clearUserCache();
+}
