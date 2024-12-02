29ced56fa025e8bb38622b88d90ab920aa903989
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-12-02 21:12:26 +0100

    updated hostname

--- Webpage/DB/dataSource.ts
+++ Webpage/DB/dataSource.ts
@@ -1,7 +1,7 @@
 import {DataSource} from "typeorm";
 export const dataSource: DataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "127.0.0.1",
     port: 3306,
     username: "waterfall_swe",
     password: "Sonntag",
