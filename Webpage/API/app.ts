dc717a44b249e7bdbc49cadb3e78b5a24eabf987
Arman <130306534+SSJGodGogeta@users.noreply.github.com>
2024-12-02 22:26:25 +0100

    navigation.ts fix ?

--- Webpage/API/app.ts
+++ Webpage/API/app.ts
@@ -40,8 +40,8 @@ console.log("Serving static files from:", path.join(__dirname, "../../features")
 app.use('/Webpage/style.css', express.static(path.join(__dirname, "../../style.css")));
 console.log("Serving CSS from:", path.join(__dirname, "../../style.css"));
 // navigation.ts /Webpage/dist/navigation.js
-app.use('/Webpage/dist/navigation.js', express.static(path.join(__dirname, "../navigation.js")));
-console.log("Serving navigation.js from:", path.join(__dirname, "../navigation.js"));
+app.use('/Webpage/dist/navigation.js', express.static(path.join(__dirname, "../")));
+console.log("Serving navigation.js from:", path.join(__dirname, "../"));
 // /Webpage/assets/logo.png
 app.use('/Webpage/assets/', express.static(path.join(__dirname, "../../assets/")));
 console.log("Serving assets from:", path.join(__dirname, "../../assets/"));
